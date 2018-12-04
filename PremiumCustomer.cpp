24a96551d7b319d57c133765f3459556ac30c03d
aytona <christopher.aytona@gmail.com>
2018-12-04 09:42:25 -0500

    Another big fix

--- PremiumCustomer.cpp
+++ PremiumCustomer.cpp
@@ -5,7 +5,7 @@
 void PremiumCustomer::computeBalance() {
     double balance = 0;
     for (unsigned int i = 0; i < this->m_Calls.size(); ++i) {
-		balance += (double)this->m_Calls.at(i)->getDuration() / 60 * this->m_minRate;
+        balance += (double)this->m_Calls.at(i)->getDuration() / 60 * this->m_minRate;
     }
     setBalance(balance + this->m_fee);
 }
